4a11b1b5033d3690521c3dde61dd30c9269aedc9
Syedzainali777 <ali4205359@gmail.com>
2024-04-07 12:49:57 +0500

    addExpense value

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -7,7 +7,7 @@ import "./Expense.css";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
-  const [filterInfoText, setFilterInfoText] = useState('2019, 2021, 2022');
+  const [filterInfoText, setFilterInfoText] = useState("2019, 2021, 2022");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
@@ -17,7 +17,7 @@ const Expenses = (props) => {
       setFilterInfoText("2019, 2021, 2022");
     } else if (selectedYear === "2021") {
       setFilterInfoText("2019, 2020, 2022");
-    } else{
+    } else {
       setFilterInfoText("2019, 2020, 2021");
     }
   };
@@ -30,26 +30,14 @@ const Expenses = (props) => {
           onChangeFilter={filterChangeHandler}
         />
         <p>Data for the years {filterInfoText} is hidden</p>
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            key={expense.id} // Adding key prop with a unique identifier
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
